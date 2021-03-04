f86b23d0529d531c27347254ab1d1cc21f2e749f
094-gengar <koyamagen.genkoyama@gmail.com>
2021-03-04 15:51:29 +0900

    little fix

--- math/primefact.hpp
+++ math/primefact.hpp
@@ -8,11 +8,7 @@ template<typename T>vector<pair<T,T>>prime_factor(T n)
 	{
 		if(n%i!=0)continue;
 		T tmp=0;
-		while(n%i==0)
-		{
-			tmp++;
-			n/=i;
-		}
+		while(n%i==0){tmp++;n/=i;}
 		ret.push_back(make_pair(i,tmp));
 	}
 	if(n!=1)ret.push_back(make_pair(n,1));
