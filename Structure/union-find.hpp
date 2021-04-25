dce636e7d1233ad4cd6d5e39b7d61fbe2eeb07c8
094-gengar <koyamagen.genkoyama@gmail.com>
2021-04-25 11:42:36 +0900

    add groupcount to unionfind

--- Structure/union-find.hpp
+++ Structure/union-find.hpp
@@ -3,7 +3,8 @@
 struct uni
 {
 	vector<int>par,siz;
-	uni(int n):par(n),siz(n,1LL)
+	int g_;
+	uni(int n):par(n),siz(n,1LL),g_(n)
 	{
 		for(int i=0;i<n;i++)par[i]=i;
 	}
@@ -18,13 +19,14 @@ struct uni
 		int rx=root(x);
 		int ry=root(y);
 		if(rx==ry)return;
+		g_--;
 		if(siz[rx]<siz[ry])swap(rx,ry);
 		siz[rx]+=siz[ry];
 		par[ry]=rx;
 		return;
 	}
+	int grcount(){return g_;}
 	int root(int x){return par[x]==x?x:par[x]=root(par[x]);}
 	bool same(int x,int y){return root(x)==root(y);}
 	int size(int x){return siz[root(x)];}
 };
-
