42678eff0facd7ec2335dba9627cb2051f68d349
094-gengar <koyamagen.genkoyama@gmail.com>
2021-06-20 12:16:44 +0900

    fix modint

--- math/modint.hpp
+++ math/modint.hpp
@@ -1,43 +1,41 @@
 template<long long Mod>struct modInt
 {
 	long long x;
-	constexpr modInt(long long v=0)noexcept:x(v%Mod){if(x<0)x+=Mod;}
+	constexpr modInt()noexcept:x(){}
 	template<typename T>
-	constexpr long long getval()const noexcept {return x;}
-	constexpr long long getmod(){return Mod;}
+	constexpr modInt(T v=0)noexcept:x(v%Mod){if(x<0)x+=Mod;}
+	constexpr long long getval()const noexcept{return x;}
 	constexpr modInt operator-()const noexcept{return x?Mod-x:0;}
 	constexpr modInt operator+(const modInt&r)const noexcept{return modInt(*this)+=r;}
 	constexpr modInt operator-(const modInt&r)const noexcept{return modInt(*this)-=r;}
 	constexpr modInt operator*(const modInt&r)const noexcept{return modInt(*this)*=r;}
 	constexpr modInt operator/(const modInt&r)const noexcept{return modInt(*this)/=r;}
-	constexpr modInt&operator+=(const modInt&r)noexcept{x+=r.x;if(x>=Mod)x-=Mod;return *this;}
-	constexpr modInt&operator-=(const modInt&r)noexcept{x-=r.x;if(x<0)x+=Mod;return *this;}
-	constexpr modInt&operator*=(const modInt&r)noexcept{x=x*r.x%Mod;return *this;}
-	constexpr modInt&operator/=(const modInt&r)noexcept
+	constexpr modInt&operator+=(const modInt&r)noexcept{x+=r.x;if(x>=Mod)x-=Mod;return*this;}
+	constexpr modInt&operator-=(const modInt&r)noexcept{x-=r.x;if(x<0)x+=Mod;return*this;}
+	constexpr modInt&operator*=(const modInt&r)noexcept{x=x*r.x%Mod;return*this;}
+	constexpr modInt&operator/=(const modInt&r)noexcept{x=x*r.inv();return*this;}
+	constexpr modInt powm(long long n)noexcept
 	{
-		long long a=r.x,b=Mod,u=1,v=0;
+		if(n<0)return powm(-n).inv();
+		modInt x=*this,r=1;
+		for(;n;x*=x,n>>=1)if(n&1)r*=x;
+		return r;
+	}
+	constexpr modInt inv()const noexcept
+	{
+		long long a=x,b=Mod,u=1,v=0;
 		while(b)
 		{
 			long long t=a/b;
 			a-=t*b;swap(a,b);
 			u-=t*v;swap(u,v);
 		}
-		x=x*u%Mod;
-		if(x<0)x+=Mod;
-		return *this;
+		return modInt(u);
 	}
 	constexpr bool operator==(const modInt&r){return this->x==r.x;}
 	constexpr bool operator!=(const modInt&r){return this->x!=r.x;}
 	friend ostream&operator<<(ostream&os,const modInt<Mod>&a){return os<<a.x;}
 	friend istream&operator>>(istream&is,modInt<Mod>&a){long long v;is>>v;a=modInt<Mod>(v);return is;} 
-	friend constexpr modInt<Mod>modpow(const modInt<Mod>&a,long long n)noexcept
-	{
-		if(a==0)return 1;
-		auto t=modpow(a,n/2);
-		t=t*t;
-		if(n&1)t=t*a;
-		return t;
-	}
 };
 
 //const long long mod=1000000007;
