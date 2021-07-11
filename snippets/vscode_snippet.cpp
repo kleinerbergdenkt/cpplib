d5b180f01b1b7cb9e2695bcd46783a36a45dce7d
094-gengar <koyamagen.genkoyama@gmail.com>
2021-07-11 20:08:12 +0900

    update snippets

--- snippets/vscode_snippet.cpp
+++ snippets/vscode_snippet.cpp
@@ -58,87 +58,91 @@
 #define bitshift(n) (1LL<<(n))
 #define myceil(a,b) ((a)+((b)-1))/(b)
 #define scale(n) cout<<fixed<<setprecision(n)
-namespace gengar094
+namespace gengar094{
+
+using i32=std::int32_t;
+using i64=std::int64_t;
+using u32=std::uint32_t;
+using u64=std::uint64_t;
+using isize=std::ptrdiff_t;
+using usize=std::size_t;
+using ll=long long;
+using ull=unsigned long long;
+
+int Scan(){return getchar();}
+void Scan(signed&a){scanf("%d",&a);}
+void Scan(unsigned&a){scanf("%u",&a);}
+void Scan(long&a){scanf("%ld",&a);}
+void Scan(long long&a){scanf("%lld",&a);}
+void Scan(unsigned long long&a){scanf("%llu",&a);}
+void Scan(char&a){do{a=getchar();}while(a==' '||a=='\n');}
+void Scan(float&a){scanf("%f",&a);}
+void Scan(double&a){scanf("%lf",&a);}
+void Scan(long double&a){scanf("%Lf",&a);}
+void Scan(std::string&a){std::cin>>a;}
+template<class T>void Scan(std::vector<T>&);
+template<class T,class U>void Scan(std::pair<T,U>&);
+template<class T>void Scan(std::vector<T>&a){for(auto&&i:a)Scan(i);}
+template<class T,class U>void Scan(std::pair<T,U>&a){Scan(a.first);Scan(a.second);}
+template<class T>void Scan(T&a){std::cin>>a;}
+void in(){}
+template<class Car,class...Cdr>void in(Car&&car,Cdr&&...cdr){Scan(car);in(std::forward<Cdr>(cdr)...);}
+
+void Print(){putchar(' ');}
+void Print(signed a){printf("%d",a);}
+void Print(bool a){printf("%d",a);}
+void Print(unsigned a){printf("%u",a);}
+void Print(long a){printf("%ld",a);}
+void Print(long long a){printf("%lld",a);}
+void Print(unsigned long long a){printf("%llu",a);}
+void Print(char a){printf("%c",a);}
+void Print(float a){printf("%f",a);}
+void Print(double a){printf("%lf",a);}
+void Print(long double a){printf("%Lf",a);}
+void Print(const std::string&a){for(auto&&i:a)Print(i);}
+template<class T>void Print(const std::vector<T>&);
+template<class T,class U>void Print(const std::pair<T,U>&);
+template<class T>void Print(const std::vector<T>&a){if(a.empty())return;Print(a[0]);for(auto i=a.begin();++i!=a.end();){putchar(' ');Print(*i);}}
+template<class T,class U>void Print(const std::pair<T,U>&a){Print(a.first);putchar(' ');Print(a.second);}
+template<class T>void Print(const T&a){std::cout<<a;}
+void out(){}
+template<class T>void out(const T&t){Print(t);putchar('\n');}
+template<class Car,class...Cdr>void out(Car&car,Cdr&...cdr){Print(car);putchar(' ');out(std::forward<Cdr>(cdr)...);}
+
+void yn(bool fl=true){out(fl?"Yes":"No");}
+template<class T>void drop(T x){std::cout<<(x)<<std::endl;exit(0);}
+void dYes(){std::flush(std::cout);puts("Yes");std::flush(std::cout);exit(0);}
+void dNo(){std::flush(std::cout);puts("No");std::flush(std::cout);exit(0);}
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+int lcm(int a,int b){return a/gcd(a,b)*b;}
+int fact(int n,int m){int f=n;for(int i=n-1;i>=1;i--){f*=i;f%=m;}return f;}
+template<typename T>std::vector<std::pair<T,T>>prime_factor(T n)
 {
-	using i32=std::int32_t;
-	using i64=std::int64_t;
-	using u32=std::uint32_t;
-	using u64=std::uint64_t;
-	using isize=std::ptrdiff_t;
-	using usize=std::size_t;
-	using ll=long long;
-	using ull=unsigned long long;
-	
-	int Scan(){return getchar();}
-	void Scan(signed&a){scanf("%d",&a);}
-	void Scan(unsigned&a){scanf("%u",&a);}
-	void Scan(long&a){scanf("%ld",&a);}
-	void Scan(long long&a){scanf("%lld",&a);}
-	void Scan(unsigned long long&a){scanf("%llu",&a);}
-	void Scan(char&a){do{a=getchar();}while(a==' '||a=='\n');}
-	void Scan(float&a){scanf("%f",&a);}
-	void Scan(double&a){scanf("%lf",&a);}
-	void Scan(long double&a){scanf("%Lf",&a);}
-	void Scan(std::string&a){std::cin>>a;}
-	template<class T>void Scan(std::vector<T>&);
-	template<class T,class U>void Scan(std::pair<T,U>&);
-	template<class T>void Scan(std::vector<T>&a){for(auto&&i:a)Scan(i);}
-	template<class T,class U>void Scan(std::pair<T,U>&a){Scan(a.first);Scan(a.second);}
-	template<class T>void Scan(T&a){std::cin>>a;}
-	void in(){}
-	template<class Car,class...Cdr>void in(Car&&car,Cdr&&...cdr){Scan(car);in(std::forward<Cdr>(cdr)...);}
-	
-	void Print(){putchar(' ');}
-	void Print(signed a){printf("%d",a);}
-	void Print(bool a){printf("%d",a);}
-	void Print(unsigned a){printf("%u",a);}
-	void Print(long a){printf("%ld",a);}
-	void Print(long long a){printf("%lld",a);}
-	void Print(unsigned long long a){printf("%llu",a);}
-	void Print(char a){printf("%c",a);}
-	void Print(float a){printf("%f",a);}
-	void Print(double a){printf("%lf",a);}
-	void Print(long double a){printf("%Lf",a);}
-	void Print(const std::string&a){for(auto&&i:a)Print(i);}
-	template<class T>void Print(const std::vector<T>&);
-	template<class T,class U>void Print(const std::pair<T,U>&);
-	template<class T>void Print(const std::vector<T>&a){if(a.empty())return;Print(a[0]);for(auto i=a.begin();++i!=a.end();){putchar(' ');Print(*i);}}
-	template<class T,class U>void Print(const std::pair<T,U>&a){Print(a.first);putchar(' ');Print(a.second);}
-	template<class T>void Print(const T&a){std::cout<<a;}
-	void out(){}
-	template<class T>void out(const T&t){Print(t);putchar('\n');}
-	template<class Car,class...Cdr>void out(Car&car,Cdr&...cdr){Print(car);putchar(' ');out(std::forward<Cdr>(cdr)...);}
-	
-	void yn(bool fl=true){out(fl?"Yes":"No");}
-	template<class T>void drop(T x){std::cout<<(x)<<std::endl;exit(0);}
-	void dYes(){std::flush(std::cout);puts("Yes");std::flush(std::cout);exit(0);}
-	void dNo(){std::flush(std::cout);puts("No");std::flush(std::cout);exit(0);}
-	int gcd(int a,int b){return b?gcd(b,a%b):a;}
-	int lcm(int a,int b){return a/gcd(a,b)*b;}
-	int fact(int n,int m){int f=n;for(int i=n-1;i>=1;i--){f*=i;f%=m;}return f;}
-	template<typename T>std::vector<std::pair<T,T>>prime_factor(T n)
+	std::vector<std::pair<T,T>>ret;
+	for(T i=2;i*i<=n;i++)
 	{
-		std::vector<std::pair<T,T>>ret;
-		for(T i=2;i*i<=n;i++)
-		{if(n%i!=0)continue;T tmp=0;
-		while(n%i==0){tmp++;n/=i;}ret.push_back(make_pair(i,tmp));}
-		if(n!=1)ret.push_back(make_pair(n,1));
-		return ret;
+		if(n%i!=0)continue;
+		T tmp=0;
+		while(n%i==0){tmp++;n/=i;}
+		ret.push_back(make_pair(i,tmp));
 	}
-	template<class T>int chmin(T&a,const T&b){if(b<a){a=b;return 1;}return 0;}
-	template<class T>int chmax(T&a,const T&b){if(a<b){a=b;return 1;}return 0;}
-	const ll inf=0x1fffffffffffffff;
-	const double pi=acos(-1);
-	const int mod=1000000007;//998244353
-	
-	auto _main=[]()
-	{
-		using namespace std;
-		
-	};
+	if(n!=1)ret.push_back(make_pair(n,1));
+	return ret;
 }
+template<class T>int chmin(T&a,const T&b){if(b<a){a=b;return 1;}return 0;}
+template<class T>int chmax(T&a,const T&b){if(a<b){a=b;return 1;}return 0;}
+const ll inf=0x1fffffffffffffff;
+const double pi=acos(-1);
+const int mod=1000000007;//998244353
+
+auto _main=[]()
+{
+	using namespace std;
+	
+};
 
 
+}
 std::int32_t main()
 {
 	//std::ios::sync_with_stdio(false);
