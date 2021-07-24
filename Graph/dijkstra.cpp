14d29d7b481eca03b858f55e002e138f1d8369e7
094-gengar <koyamagen.genkoyama@gmail.com>
2021-07-24 11:49:54 +0900

    update dijkstra

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,47 +1,36 @@
-#include<algorithm>
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-#define int long long
-#define P pair<int,int>
-template<class T>int chmin(T&a,const T&b){if(b<a){a=b;return 1;}return 0;}
+#include<bits/stdc++.h>
 
-void dijkstra()
+int n,m,s,t;
+std::vector<std::vector<std::pair<int,int>>>g(101010);
+
+int dijkstra(int s,int t)
 {
-	int n,m,s,t;cin>>n>>m>>s>>t;
-	vector<vector<P>>g(n);
-	for(int i=0;i<m;i++)
-	{
-		int a,b,cst;cin>>a>>b>>cst;
-		g[a].push_back(make_pair(b,cst));
-	}
-	vector<int>dist(n,(1LL<<60));dist[s]=0;
-	vector<int>prev(n,-1);
-	priority_queue<P,vector<P>,greater<P>>pq;
-	pq.push(make_pair(dist[s],s));
+	std::vector<int>dist(n,1e9);dist[s]=0;
+	std::priority_queue<std::pair<int,int>,
+	std::vector<std::pair<int,int>>,
+	std::greater<>>pq;
+	pq.emplace(dist[s],s);
 	while(!pq.empty())
 	{
-		auto[d,v]=pq.top();
+		auto[x,u]=pq.top();
 		pq.pop();
-		if(d>dist[v])continue;
-		for(auto e:g[v])if(chmin(dist[e.first],dist[v]+e.second))
-		{
-			prev[e.first]=v;
-			pq.push(make_pair(dist[e.first],e.first));
-		}
+		if(x>dist[u])continue;
+		for(auto[v,c]:g[u])c+=dist[u],dist[v]>c&&(pq.emplace(dist[v]=c,v),0);
 	}
-	vector<int>keiro;
-	for(int cur=t;cur!=-1;cur=prev[cur])keiro.push_back(cur);
-	reverse(keiro.begin(),keiro.end());
-	if(dist[t]==1LL<<60)cout<<-1<<' ';
-	else
-	{
-		cout<<dist[t]<<' ';
-		int size_of_keiro=keiro.size()-1;
-		cout<<size_of_keiro<<endl;
-		for(int i=0;i<size_of_keiro;i++)cout<<keiro[i]<<' '<<keiro[i+1]<<endl;
-	}
+	return dist[t];
 }
 
-main(){dijkstra();}
+int main()
+{
+	using namespace std;
+	cin>>n>>m>>s>>t;
+	s--;t--;
+	for(int i=0,a,b,c;i<m;i++)
+	{
+		cin>>a>>b>>c;a--;b--;
+		g[a].emplace_back(b,c);
+		g[b].emplace_back(a,c);
+	}
+
+	cout<<dijkstra(s,t);
+}
