08c78b435a5bb7b2230eaa11efb27bb57527d288
094-gengar <koyamagen.genkoyama@gmail.com>
2021-08-13 18:38:21 +0900

    fix snippets

--- snippets/snippet2.cpp
+++ snippets/snippet2.cpp
@@ -36,13 +36,13 @@
 #pragma GCC target("avx512f")
 #pragma GCC optimize("O3")
 #define int long long
-#define L long long
+#define li long long
 #define itn int
 #define INT(...)     \
 	int __VA_ARGS__; \
 	in(__VA_ARGS__)
 #define LI(...)    \
-	L __VA_ARGS__; \
+	li __VA_ARGS__; \
 	in(__VA_ARGS__)
 #define I32(...)     \
 	i32 __VA_ARGS__; \
@@ -99,8 +99,8 @@ using isize = std::ptrdiff_t;
 using usize = std::size_t;
 using vi = std::vector<int>;
 using vvi = std::vector<vi>;
-using vL = std::vector<L>;
-using vvL = std::vector<vL>;
+using vli = std::vector<li>;
+using vvli = std::vector<vli>;
 using vi32 = std::vector<i32>;
 using vvi32 = std::vector<vi32>;
 using vi64 = std::vector<i64>;
@@ -113,7 +113,7 @@ using vc = std::vector<char>;
 using vvc = std::vector<vc>;
 using vs = std::vector<std::string>;
 using vpii = std::vector<std::pair<int, int>>;
-using vpLL = std::vector<std::pair<L, L>>;
+using vpli = std::vector<std::pair<li, li>>;
 using P = std::pair<int, int>;
 
 template <class T>
@@ -298,8 +298,8 @@ std::vector<std::pair<T, T>> prime_factor(T n)
 		ret.push_back(std::make_pair(n, 1));
 	return ret;
 }
-constexpr L inf = 0x1fffffffffffffff;
-constexpr L mod = 1000000007LL; //998244353
+constexpr li inf = 0x1fffffffffffffff;
+constexpr li mod = 1000000007LL; //998244353
 constexpr double eps = 1e-8;
 constexpr double pi = 3.141592653589793238462643383279;
 
